67e247dc04002d2fbb5d70738efcd42933713c86
gichang-yang <jaeho8032@gmail.com>
2016-05-21 23:55:53 +0900

    ch9

---
+++ ch9/one.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+extern int two();
+int main(void)
+{
+	printf("Happy Linux Study.\n");
+	two();
+	system("clear");
+}
---
+++ ch9/two.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int two()
+{
+	printf("Managing Software.\n");
+	printf("Make Makefile.\n");
+	printf("Good Bye.\n");
+	return 0;
+}
